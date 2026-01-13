c1d6f2719b1787c0d272627b92abfecd5d182536
CP3 <944122095@qq.com>
2026-01-13 22:29:57 +0800

    Update FixedPointMath.cs

--- Assets/Project/Scripts/Code/Math/FixedPointMath.cs
+++ Assets/Project/Scripts/Code/Math/FixedPointMath.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FixedPointMath
+public static class FixedPointMath
 {
-    
+    public const int LUT_SIZE = 3600;//定义查找表的大小 (0.1度精度，360度共3600个值)
+
 }
