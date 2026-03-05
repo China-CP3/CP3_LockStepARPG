e1d296711bb615f03dab6c6863a206c5fc45ba83
CP3 <944122095@qq.com>
2026-03-05 17:00:42 +0800

    Update Collider2DBox.cs

--- Assets/Project/Scripts/Physics/Physics2D/Colliders/Collider2DBox.cs
+++ Assets/Project/Scripts/Physics/Physics2D/Colliders/Collider2DBox.cs
@@ -24,6 +24,27 @@ public class Collider2DBox : Collider2DBase
 
     public override Collider2DBox GenerateSweptAABB(FixedPointVector2 lastFramePos, FixedPointVector2 curFramePos)
     {
-        return null;
+        //根据2个box 求一个大的能包裹2个box的大box 
+
+        //求上帧位置和当前帧位置里 最小的中心点 和最大的中心点
+        FixedPoint minCenterX = FixedPointMath.Min(lastFramePos.x, curFramePos.x);
+        FixedPoint minCenterY = FixedPointMath.Min(lastFramePos.y, curFramePos.y);
+        FixedPoint maxCenterX = FixedPointMath.Max(lastFramePos.x, curFramePos.x);
+        FixedPoint maxCenterY = FixedPointMath.Max(lastFramePos.y, curFramePos.y);
+
+        //计算新的box上 最左边的点 最右边的点 最高的点 最低的点 或者说box的四条边
+        FixedPoint newBoxMinX = minCenterX - HalfWidth;
+        FixedPoint newBoxMaxX = maxCenterX + HalfWidth;
+        FixedPoint newBoxMinY = minCenterY - HalfHeight;
+        FixedPoint newBoxMaxY = maxCenterY + HalfHeight;
+
+        FixedPoint newWidth = newBoxMaxX - newBoxMinX;
+        FixedPoint newHight = newBoxMaxY - newBoxMinY;
+        FixedPoint newCenterX = (newBoxMaxX + newBoxMinX) / FixedPoint.CreateByInt(2);//不是宽度直接除以2 是左右坐标相加除以2 比如 -2 + 4 的一半 = 1  按宽带除以2就是3了
+        FixedPoint newCenterY = (newBoxMinY + newBoxMaxY) / FixedPoint.CreateByInt(2);//不是宽度直接除以2 是左右坐标相加除以2 比如 -2 + 4 的一半 = 1  按宽带除以2就是3了
+
+        FixedPointVector2 newCenter = new FixedPointVector2(newCenterX, newCenterY);
+
+        return new Collider2DBox(newCenter, newCenter,new FixedPointVector2(newWidth, newHight));
     }
 }
