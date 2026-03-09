bc4ec19a8b79eb4c4756c7cc67a408798d5b64c0
CP3 <944122095@qq.com>
2026-03-09 22:24:57 +0800

    Update Collider2DCircle.cs

--- Assets/Project/Scripts/Physics/Physics2D/Colliders/Collider2DCircle.cs
+++ Assets/Project/Scripts/Physics/Physics2D/Colliders/Collider2DCircle.cs
@@ -19,6 +19,9 @@ public class Collider2DCircle : Collider2DBase
 
     public override Collider2DBox GenerateSweptAABB(FixedPointVector2 lastFramePos, FixedPointVector2 curFramePos)
     {
+        //依然返回一个box出去 不是圆 不然用椭圆和box做扫描检测 复杂度增加 性能不好 完全可以接受用这一点误差换开发进度和性能
+
+
         return null;
     }
 }
