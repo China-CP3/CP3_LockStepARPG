dd18e42509fe0770d17c3b4f6ddc6d9d938cbdb3
CP3 <944122095@qq.com>
2026-03-22 22:22:48 +0800

    Update QuadTree2D.cs

--- Assets/Project/Scripts/Physics/Physics2D/QuadTree2D.cs
+++ Assets/Project/Scripts/Physics/Physics2D/QuadTree2D.cs
@@ -5,6 +5,6 @@ public class QuadTree2D
 {
     private int MAX_Objs = 4;//单个节点 最多容纳4个物体
     private int MAx_Level = 5;//最多分裂5层
-    //todo 考虑泛型支持容器list
+    //todo 考虑泛型支持容器list  也可以考虑用接口定义方法 更通用 但目前demo貌似只需要支持碰撞器即可 看时间够不够把
 
 }
