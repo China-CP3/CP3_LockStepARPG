7a195830be5cc704d8924e1497b8100cba7147a7
CP3 <944122095@qq.com>
2026-03-23 22:40:45 +0800

    Update QuadTree2D.cs

--- Assets/Project/Scripts/Physics/Physics2D/QuadTree2D.cs
+++ Assets/Project/Scripts/Physics/Physics2D/QuadTree2D.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuadTree2D
+public class QuadTree2D <T> where T : Collider2DBase
 {
     private int MAX_Objs = 4;//单个节点 最多容纳4个物体
     private int MAx_Level = 5;//最多分裂5层
-    //todo 考虑泛型支持容器list  也可以考虑用接口定义方法 更通用 但目前demo貌似只需要支持碰撞器即可 看时间够不够把
+    private List<T> nodeList = new List<T>();//所有的节点
+
 
 }
