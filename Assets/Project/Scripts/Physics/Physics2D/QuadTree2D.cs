b60b4f174f23d5cc0b86c9c29a2d9157b618a9d2
CP3 <944122095@qq.com>
2026-03-23 22:50:04 +0800

    Update QuadTree2D.cs

--- Assets/Project/Scripts/Physics/Physics2D/QuadTree2D.cs
+++ Assets/Project/Scripts/Physics/Physics2D/QuadTree2D.cs
@@ -1,11 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuadTree2D <T> where T : Collider2DBase
+public class QuadTree2D<T> where T : Collider2DBase,new()
 {
-    private int MAX_Objs = 4;//单个节点 最多容纳4个物体
-    private int MAx_Level = 5;//最多分裂5层
+    private const int MAX_Objs = 4;//单个节点 最多容纳4个物体
+    private const int MAx_Level = 5;//最多分裂5层
+    private int level;// 当前层级
     private List<T> nodeList = new List<T>();//所有的节点
+    private QuadTree2D<T>[] children = new QuadTree2D<T>[MAX_Objs];
 
+    private FixedPointVector2 center;
+    private FixedPoint width;
+    private FixedPoint height;
 
+   
 }
