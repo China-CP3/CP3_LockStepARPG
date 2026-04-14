30c4cd47ca8513d7b8f45f81f1a1ffb47ce91205
CP3 <944122095@qq.com>
2026-04-14 22:05:25 +0800

    1

--- Assets/Project/ActDemo/Scripts/Character.cs
+++ Assets/Project/ActDemo/Scripts/Character.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     private Animator animator;
+    private float speed = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -21,20 +22,25 @@ public class Character : MonoBehaviour
         if (isWPressed && isShiftPressed)
         {
             // W + Shift 按下，播放Run动画
-            animator.SetBool("Walk", false);
-            animator.SetBool("Run", true);
+            //animator.SetBool("Walk", false);
+            //animator.SetBool("Run", true);
+            speed -= Time.deltaTime;
+            animator.SetFloat("Speed", speed);
         }
         else if (isWPressed)
         {
             // 只按下W，播放Walk动画
-            animator.SetBool("Walk", true);
-            animator.SetBool("Run", false);
+            //animator.SetBool("Walk", true);
+            //animator.SetBool("Run", false);
+            speed += Time.deltaTime;
+            animator.SetFloat("Speed", speed);
         }
         else
         {
             // 没有按下W，播放Idle动画
-            animator.SetBool("Walk", false);
-            animator.SetBool("Run", false);
+            //animator.SetBool("Walk", false);
+            //animator.SetBool("Run", false);
+            
         }
     }
 }
