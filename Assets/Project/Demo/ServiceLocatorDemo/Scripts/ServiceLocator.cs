fadf03ff3eb57f013e1b2afdcd6e3ee390546ef8
CP3 <944122095@qq.com>
2026-09-05 23:59:03 +0800

    Update ServiceLocator.cs

--- Assets/Project/Demo/ServiceLocatorDemo/Scripts/ServiceLocator.cs
+++ Assets/Project/Demo/ServiceLocatorDemo/Scripts/ServiceLocator.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System;
+using UnityEngine;
 
 namespace CP3.Demo.ServiceLocator
 {
@@ -7,5 +8,14 @@ namespace CP3.Demo.ServiceLocator
     {
         private static readonly Dictionary<Type, object> s_Services = new Dictionary<Type, object>();
 
+        public static void RegisterService<T>(T service) where T:class
+        {
+            if(service == null)
+            {
+                Debug.LogErrorFormat("RegisterService: service is null Type:{0}", typeof(T).Name);
+                return;
+            }
+            
+        }
     }
 }
