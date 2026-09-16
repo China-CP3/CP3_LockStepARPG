1ecf2fb5d82e500f68c54e210841368d93564e05
CP3 <944122095@qq.com>
2026-09-16 22:39:28 +0800

    Update ServiceLocator.cs

--- Assets/Project/Demo/ServiceLocatorDemo/Scripts/ServiceLocator.cs
+++ Assets/Project/Demo/ServiceLocatorDemo/Scripts/ServiceLocator.cs
@@ -12,10 +12,32 @@ namespace CP3.Demo.ServiceLocator
         {
             if(service == null)
             {
-                Debug.LogErrorFormat("RegisterService: service is null Type:{0}", typeof(T).Name);
+                //typeof(T).Name 每次调用会不会有开销？不会，typeof(T) 对同一个 T 全程返回同一个 Type 实例（CLR 缓存的），.Name 也是缓存字符串，零 GC。
+                Debug.LogErrorFormat("RegisterService: service is null Type:{0}", typeof(T));
                 return;
             }
-            
+
+            Type type = typeof(T);
+            if (s_Services.ContainsKey(type))
+            {
+                Debug.LogErrorFormat("RegisterService: service has Contains:{0}", type.Name);
+                return;
+            }
+
+            s_Services.Add(type, service);//两种方式 Add(key, value) 已存在会抛异常  [key] = value 已存在会静默覆盖
+
         }
+
+        public static T GetService<T>() where T:class
+        {
+            Type type = typeof(T);
+            if (s_Services.TryGetValue(type,out object service))
+            {
+                return service as T;
+            }
+            Debug.LogErrorFormat("GetService: not registered Type:{0}", type.Name);
+            return null;
+        }
+
     }
 }
